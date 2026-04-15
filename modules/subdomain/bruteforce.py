1df7275ae7f69b80ea6f3629ec2db38d96cc2fe6
BR45H <275584516+BR45H@users.noreply.github.com>
2026-04-15 00:01:25 -0300

    implementation of module bruteforce

--- modules/subdomain/bruteforce.py
+++ modules/subdomain/bruteforce.py
@@ -1,10 +1,35 @@
+from core.target import normalize_target_input
+from core.exceptions import ValidationError
+from core import output
+from pathlib import Path
 import argparse
+import socket
 
 def run(args: argparse.Namespace) -> None:
-    print("[subdomain:bruteforce]")
-    print(f"target   = {args.target}")
-    print(f"wordlist = {args.wordlist}")
-    print(f"verbose  = {args.verbose}")
+    target_data = normalize_target_input(args.target)
+    wordlist_subdomain = [line.strip() for line in Path.open(args.wordlist, mode="r", encoding="utf-8") if line.strip()]
+    verbose_mode = args.verbose
+    show_ipv4 = args.show_ipv4
+
+    if target_data.kind != "domain":
+        raise ValidationError("subdomain bruteforce only accepts domain targets")
+
+    if target_data.source == "file":
+        output.warning("Running 'subdomain bruteforce' against a target file may produce a large amount of output")
+
+    for target in target_data.targets:
+        output.info(f"Initiating subdomain bruteforce in {target}")
+        result = subdomain_bruteforce(target, wordlist_subdomain, verbose_mode)
+
+        if not result:
+            output.warning(f"No subdomains found for {target}")
+            continue
+        
+        for full_domain, ip in result:
+            if show_ipv4:
+                output.success(f"{full_domain} -> {ip}")
+            else:
+                output.success(f"{full_domain} exist!")
 
 def register(subparsers: argparse._SubParsersAction) -> None:
     parser = subparsers.add_parser(
@@ -26,4 +51,24 @@ def register(subparsers: argparse._SubParsersAction) -> None:
         action="store_true",
         help="Enable verbose output."
     )
+    parser.add_argument(
+        "--show-ipv4",
+        action="store_true",
+        help="Show ipv4 from subdomain found."
+    )
     parser.set_defaults(func=run)
+
+def subdomain_bruteforce(target, wordlist_subdomain: list[str], verbose: bool = False) -> list[tuple[str, str]]:
+    result = []
+    for subdomain in wordlist_subdomain:
+        full_domain = f"{subdomain}.{target}"
+        try:
+            ip = socket.gethostbyname(full_domain)
+            result.append((full_domain, ip))
+        
+        except socket.gaierror:
+            if verbose:
+                output.warning(f"Failed to resolve {full_domain}")
+            continue
+        
+    return result
